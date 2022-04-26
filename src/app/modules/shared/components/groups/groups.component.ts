3969db94d34213f8c2e148e75b42e4e1a1a76ed8
Alex Logashov <admin@shiftarray.com>
2022-04-25 17:34:46 -0700

    Fix panel content overflow

--- src/app/modules/shared/components/groups/groups.component.ts
+++ src/app/modules/shared/components/groups/groups.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
 import { restoreTabs, TabGroup } from '@lib';
 import { TabService } from 'src/app/services';
 
@@ -19,6 +20,30 @@ export class GroupsComponent {
    * List of tab groups to render.
    */
   @Input() groups: TabGroup[];
+
+  /**
+   * Expansion panel list that are part of the DOM.
+   */
+  @ViewChildren(MatExpansionPanel) private panelList: QueryList<MatExpansionPanel>;
+
+  /**
+   * Returns index of expanded panel.
+   * ℹ️ This will be used to display controls for each panel.
+   * 🤔 TODO: Find more optimized way to show controls without loops or panel list.
+   */
+  get currentPanelIndex(): number {
+    if (this.panelList) {
+      const panels = this.panelList.toArray();
+      for (let i = 0; i < panels.length; i++) {
+        if (panels[i].expanded) {
+          return i;
+        }
+      }
+    }
+
+    return -1;
+  }
+
 
   constructor(private tabService: TabService) {}
 
