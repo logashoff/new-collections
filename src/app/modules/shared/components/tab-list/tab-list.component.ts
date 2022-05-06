78e34c31ad6580108277d526e86acdbf7d0e337b
Alex Logashov <admin@shiftarray.com>
2022-05-06 11:09:13 -0700

    feat: Option to rename individual tabs
    fixes #15

--- src/app/modules/shared/components/tab-list/tab-list.component.ts
+++ src/app/modules/shared/components/tab-list/tab-list.component.ts
@@ -1,7 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewEncapsulation } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { lastValueFrom } from 'rxjs';
 import { TabService } from 'src/app/services';
-import { Tab } from 'src/app/utils';
+import { BrowserTab } from 'src/app/utils';
+import { RenameDialogComponent } from '../rename-dialog/rename-dialog.component';
 
+/**
+ * @description
+ *
+ * Tabs list component
+ */
 @Component({
   selector: 'app-tab-list',
   templateUrl: './tab-list.component.html',
@@ -10,13 +18,36 @@ import { Tab } from 'src/app/utils';
   encapsulation: ViewEncapsulation.None,
 })
 export class TabListComponent {
-  @Input() tabs: Tab[];
+  /**
+   * Tabs list to display
+   */
+  @Input() tabs: BrowserTab[];
 
+  /**
+   * Group ID tab list belongs to.
+   */
   @Input() groupId: string;
 
-  constructor(private tabService: TabService) {}
+  constructor(private tabService: TabService, private dialog: MatDialog, private cdr: ChangeDetectorRef) {}
 
-  async removeTab(tab: Tab) {
+  /**
+   * Opens dialog to rename specified tab.
+   */
+  async renameTab(tab: BrowserTab) {
+    const dialogRef = this.dialog.open(RenameDialogComponent, { data: tab, disableClose: true });
+    const updatedTitle = await lastValueFrom(dialogRef.afterClosed());
+
+    if (updatedTitle && tab.title !== updatedTitle) {
+      tab.title = updatedTitle;
+      this.tabService.saveTabs();
+      this.cdr.markForCheck();
+    }
+  }
+
+  /**
+   * Removes specified tab from tab list.
+   */
+  async removeTab(tab: BrowserTab) {
     this.tabService.removeTab(this.groupId, tab);
   }
 }
