a3d99c600ec439b5f9fda7cd053241aa5c70fc1d
Alex Logashov <admin@shiftarray.com>
2022-05-22 13:39:49 -0700

    test: Fix unit tests

--- src/app/services/search.service.spec.ts
+++ src/app/services/search.service.spec.ts
@@ -1,6 +1,6 @@
 import { waitForAsync } from '@angular/core/testing';
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
-import { of, take } from 'rxjs';
+import { map, of, switchMap, take } from 'rxjs';
 import { getTabGroupsMock } from 'src/mocks';
 import { SearchService } from './search.service';
 import { TabService } from './tab.service';
@@ -24,13 +24,18 @@ describe('SearchService', () => {
   });
 
   it('should return results', waitForAsync(() => {
-    spectator.service.search('fedora');
+    of('fedora')
+      .pipe(switchMap((search) => spectator.service.fuse$.pipe(map((fuse) => fuse.search(search)))))
+      .pipe(take(1))
+      .subscribe((searchResults) => {
+        expect(searchResults).toBeDefined();
+        expect(searchResults.length).toBeGreaterThan(0);
 
-    spectator.service.searchResults$.pipe(take(1)).subscribe((searchResults) => {
-      expect(searchResults).toBeDefined();
-      expect(searchResults.length).toBeGreaterThan(0);
-      expect(searchResults[0]).toHaveProperty('title');
-      expect(searchResults[0]).toHaveProperty('url');
-    });
+        const [{ item }] = searchResults;
+        expect(item).toHaveProperty('title');
+        expect(item).toHaveProperty('url');
+        expect(item.title).toBe('Fedora');
+        expect(item.url).toBe('https://getfedora.org/');
+      });
   }));
 });
