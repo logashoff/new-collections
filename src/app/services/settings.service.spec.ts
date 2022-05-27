5596787e8452fbd6ff8f75ac05a127e9d808b0bf
Alex Logashov <admin@shiftarray.com>
2022-05-26 18:39:12 -0700

    test: Update unit tests

--- src/app/services/settings.service.spec.ts
+++ src/app/services/settings.service.spec.ts
@@ -1,4 +1,5 @@
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { MostVisitedURL, Settings } from '../utils';
 import { SettingsService } from './settings.service';
 
 describe('SettingsService', () => {
@@ -11,5 +12,47 @@ describe('SettingsService', () => {
     spectator = createService();
   });
 
-  it('should return settings', () => {});
+  it('should handle empty settings', async () => {
+    const site: MostVisitedURL = {
+      title: 'New Site',
+      url: 'https://site.com',
+    };
+
+    const newSettings = await spectator.service.ignoreSite(site);
+
+    expect(newSettings).toEqual({
+      ignoreTopSites: [site],
+    });
+  });
+
+  it('should update current settings', async () => {
+    const savedSettings: Settings = {
+      enableDevices: true,
+      enableTopSites: true,
+    };
+
+    jest.spyOn<any, any>(spectator.service, 'getSettings').mockReturnValue(savedSettings);
+
+    const updateSettings: Settings = {
+      ignoreTopSites: [
+        {
+          title: 'Site 1',
+          url: 'https://site1.org',
+        },
+      ],
+    };
+
+    const newSettings = await spectator.service.update(updateSettings);
+
+    expect(newSettings).toEqual({
+      enableDevices: true,
+      enableTopSites: true,
+      ignoreTopSites: [
+        {
+          title: 'Site 1',
+          url: 'https://site1.org',
+        },
+      ],
+    });
+  });
 });
