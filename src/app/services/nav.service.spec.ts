78b484b4d99e4d09d7c48b9ef22c2dd26685e32c
Alex Logashov <admin@shiftarray.com>
2022-05-29 19:14:59 -0700

    test: Improve code coverage

--- src/app/services/nav.service.spec.ts
+++ src/app/services/nav.service.spec.ts
@@ -2,7 +2,7 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
-import { of } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
 import { NavService } from './nav.service';
 
 describe('NavService', () => {
@@ -16,6 +16,7 @@ describe('NavService', () => {
         useValue: {
           queryParams: of({
             groupId: '7dd29b1c-dfab-44d4-8d29-76d402d24038',
+            tabId: 123,
           }),
         },
       },
@@ -38,5 +39,19 @@ describe('NavService', () => {
     spectator.service.setParams('6ab9c99e-8942-4236-ad6e-7e38c51da810', 218);
 
     expect(spectator.service['router'].navigate).toHaveBeenCalled();
+
+    spectator.service.reset();
+
+    expect(spectator.service['router'].navigate).toHaveBeenCalledTimes(2);
+  });
+
+  it('should have params set', async () => {
+    const groupIdParam = await firstValueFrom(spectator.service.paramsGroupId$);
+
+    expect(groupIdParam).toBe('7dd29b1c-dfab-44d4-8d29-76d402d24038');
+
+    const tabIdParam = await firstValueFrom(spectator.service.paramsTabId$);
+
+    expect(tabIdParam).toBe(123);
   });
 });
