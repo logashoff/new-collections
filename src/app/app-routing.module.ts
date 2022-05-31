39fd8339fc828d01c5abfc2abac4feda33972dda
Alex Logashov <admin@shiftarray.com>
2022-05-31 10:01:22 -0700

    fix: ERR_FILE_NOT_FOUND error after reloading page
    fixes #39

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'popup', loadChildren: () => import('./modules/popup/popup.module').then((m) => m.PopupModule) },
@@ -11,6 +11,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
       useHash: true,
     }),
   ],
