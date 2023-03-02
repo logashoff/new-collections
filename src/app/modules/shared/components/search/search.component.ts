cc8c4c3f0d99776b7266219098994d5c053e94cc
Alex Logashov <dev@pusharray.com>
2023-03-02 09:00:03 -0800

    Fix regression when editing items in search results
    Reset URL params when refreshing page

--- src/app/modules/shared/components/search/search.component.ts
+++ src/app/modules/shared/components/search/search.component.ts
@@ -4,8 +4,6 @@ import Fuse from 'fuse.js';
 import isNil from 'lodash/isNil';
 import {
   BehaviorSubject,
-  combineLatest,
-  delay,
   firstValueFrom,
   lastValueFrom,
   map,
@@ -75,12 +73,13 @@ export class SearchComponent implements OnInit {
   /**
    * Source for search results.
    */
-  readonly searchResults$: Observable<BrowserTabs> = combineLatest([this.navService.paramsSearch$, this.fuse$]).pipe(
-    tap(([search]) => {
+  readonly searchResults$: Observable<BrowserTabs> = this.navService.paramsSearch$.pipe(
+    tap((search) => {
       this.formGroup.get('search').setValue(search, {
         emitEvent: false,
       });
     }),
+    withLatestFrom(this.fuse$),
     map(([search, fuse]) => (search?.length > 0 ? fuse.search(search)?.map(({ item }) => item) : null)),
     shareReplay(1)
   );
@@ -96,6 +95,8 @@ export class SearchComponent implements OnInit {
   constructor(private navService: NavService) {}
 
   ngOnInit(): void {
+    this.navService.reset();
+    
     this.formGroup.valueChanges.subscribe(({ search }) => {
       if (search) {
         this.navService.search(search);
