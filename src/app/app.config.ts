0172b1d3db65e02b33770cc6a39c58b87a0b1749
Alex Logashov <dev@pusharray.com>
2024-11-03 07:30:49 -0600

    Packages clean-up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 
-import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { PreloadAllModules, provideRouter, withHashLocation, withPreloading } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(),
-    provideAnimations(),
-    provideRouter(appRoutes, withPreloading(PreloadAllModules), withHashLocation()),
-  ],
+  providers: [provideAnimations(), provideRouter(appRoutes, withPreloading(PreloadAllModules), withHashLocation())],
 };
