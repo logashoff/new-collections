4b68d3ab2216c0eaac164f0f9e77ee3456b580a3
Alex Logashov <dev@pusharray.com>
2025-06-06 21:13:13 -0500

    Increase e2e tests timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    testTimeout: 10_000,
+    retry: 3,
     watch: false,
     environment: 'puppeteer',
     globalSetup: 'node_modules/vitest-environment-puppeteer/dist/global-init.js',
