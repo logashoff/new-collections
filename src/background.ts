decdae90237ee00d740c8541d604cf603ba03fac
Alex Logashov <dev@pusharray.com>
2025-06-23 14:25:50 -0500

    Refactor background service

--- src/background.ts
+++ src/background.ts
@@ -2,15 +2,15 @@ import { addRecent, getCollections } from './app/utils/collections';
 import type { BackgroundMessage, Tab } from './app/utils/models';
 import { getNormalizedUrl } from './app/utils/utils';
 
-const onRuntimeChanges = async () => {
+async function onRuntimeChanges() {
   const collections = await getCollections();
   await chrome.action.setBadgeText({ text: collections?.length.toString() ?? '' });
-};
+}
 
 /**
  * Update recent items list by tab URL if it matches any tab IDs in saved collection
  */
-const updateRecentByUrl = async (url: string) => {
+async function updateRecentByUrl(url: string) {
   const collections = await getCollections();
 
   if (collections?.length > 0) {
@@ -38,32 +38,36 @@ const updateRecentByUrl = async (url: string) => {
       }
     }
   }
-};
+}
 
-const onTabCreate = async (tab: Tab) => {
+async function onTabCreate(tab: Tab) {
   if (tab.pendingUrl) {
     await updateRecentByUrl(tab.pendingUrl);
   }
-};
+}
 
-chrome.runtime.onInstalled.addListener(onRuntimeChanges);
-chrome.runtime.onStartup.addListener(onRuntimeChanges);
-chrome.storage.onChanged.addListener(onRuntimeChanges);
+async function onMessage(message: BackgroundMessage) {
+  const { onCreated } = chrome.tabs;
+  onCreated.removeListener(onTabCreate);
+
+  if (message.tabId) {
+    await addRecent(message.tabId);
+  } else if (message.tabUrl) {
+    await updateRecentByUrl(message.tabUrl);
+  }
+
+  onCreated.addListener(onTabCreate);
+}
+
+async function onConnect(port: chrome.runtime.Port) {
+  port.onMessage.addListener(onMessage);
+}
 
 // Helps to handle items opened using context menu
 chrome.tabs.onCreated.addListener(onTabCreate);
 
-chrome.runtime.onConnect.addListener((port) => {
-  port.onMessage.addListener(async (message: BackgroundMessage) => {
-    const { onCreated } = chrome.tabs;
-    onCreated.removeListener(onTabCreate);
+chrome.runtime.onInstalled.addListener(onRuntimeChanges);
+chrome.runtime.onStartup.addListener(onRuntimeChanges);
+chrome.runtime.onConnect.addListener(onConnect);
 
-    if (message.tabId) {
-      await addRecent(message.tabId);
-    } else if (message.tabUrl) {
-      await updateRecentByUrl(message.tabUrl);
-    }
-
-    onCreated.addListener(onTabCreate);
-  });
-});
+chrome.storage.onChanged.addListener(onRuntimeChanges);
