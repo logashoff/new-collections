c696d9caf5ef2b2e18422dd2c24b87161a929ac6
Alex Logashov <dev@pusharray.com>
2025-09-01 10:48:29 -0500

    Disable view transitions for e2e builds

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, RouterFeatures, withHashLocation, withViewTransitions } from '@angular/router';
 
-import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
-
+import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
 
+const routeFeatures: RouterFeatures[] = [withHashLocation()];
+
+if (!environment.e2e) {
+  routeFeatures.push(withViewTransitions());
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withHashLocation(), withViewTransitions()), provideZonelessChangeDetection()],
+  providers: [provideRouter(appRoutes, ...routeFeatures), provideZonelessChangeDetection()],
 };
